0cff7614f49a6ff9ced751a4951ec1c8faadc412
INFO-2 <INFO-2@DESKTOP-2CT9I6E>
2025-04-16 22:37:59 -0300

    ta todo hecho polfin

---
+++ PersonajeConsole/PersonajeConsole/Program.cs
@@ -0,0 +1,78 @@
+﻿using PersonajeConsole;
+
+Personaje p1 = new Personaje();
+Personaje p2 = new Personaje();
+Personaje personaje = new Personaje();
+
+int seleccion=3;
+Console.WriteLine("antes de comenzar crea 2 personajes");
+Console.WriteLine("Personaje numero 1");
+CargaPersonaje(p1);
+Console.WriteLine("Personaje numero 2");
+CargaPersonaje(p2);
+Console.WriteLine("Personajes cargados!, disfrute del juego :v (pulse cualquier tecla para continuar)");
+Console.ReadKey();
+
+Console.Clear();
+
+while (seleccion!=0)
+{
+   
+    Console.WriteLine("ingrese que desea hacer! (usted juega como el personaje 1)");
+    Console.WriteLine("1-cambiar color");
+    Console.WriteLine("2-atacar!");
+    Console.WriteLine("3-recibir daño");
+    Console.WriteLine("0-salir");
+    seleccion = int.Parse(Console.ReadLine());
+    switch (seleccion)
+    {
+        case 1:
+            Console.WriteLine("ingrese el nuevo color del personaje 1");
+            p1.CambiarColor(Console.ReadLine());
+
+            Console.WriteLine("Color cambiado!");
+            break;
+        case 2: 
+                Console.WriteLine("El daño causado es de: " + p1.atacar(p2)); 
+            break;
+        case 3:
+           
+            Console.WriteLine("el daño recibido es de: " + p2.atacar(p1));
+
+            break;
+        
+        
+    }
+    MuestraPJ();
+    Console.ReadKey();
+    Console.Clear();
+}
+
+void CargaPersonaje(Personaje personaje)
+{
+    Console.WriteLine("ingrese cuanta vida tiene su personaje!");
+    personaje.Vida = int.Parse(Console.ReadLine());
+    Console.WriteLine("ingrese el color del personaje");
+    personaje.Color = Console.ReadLine();
+    Console.WriteLine("ingrese la cantidad de mana de su personaje");
+    personaje.Mana = int.Parse(Console.ReadLine());
+    Console.WriteLine("ingrese cuanta defensa tiene su personaje!");
+    personaje.Defensa = int.Parse(Console.ReadLine());
+    Console.WriteLine("ingrese su fuerza!");
+    personaje.Fuerza = int.Parse(Console.ReadLine());
+
+}
+void MuestraPJ()
+{
+    Console.WriteLine("Personaje: " + p1.Color);
+    Console.WriteLine("Vida: "+p1.Vida);
+    Console.WriteLine("Mana: " +p1.Mana);
+    Console.WriteLine();
+    Console.WriteLine("----------------------------------------");
+    Console.WriteLine();
+    Console.WriteLine("Personaje: " + p2.Color);
+    Console.WriteLine("Vida: " + p2.Vida);
+    Console.WriteLine("Mana: " + p2.Mana);
+
+
+}
